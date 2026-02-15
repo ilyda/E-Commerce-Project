fd6758b06570b10564236cbfe808b802c8fe0a3b
ilyda <ilayda.aydinn97@gmail.com>
2026-02-15 13:13:35 +0300

    fix: render spa routing

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,9 +2,16 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://workintech-fe-ecommerce.onrender.com",
-   headers: {
-    Authorization: `${localStorage.getItem("token")}`
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = token;
   }
+
+  return config;
 });
 
 export default api;
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
-   plugins: [
+  plugins: [
     tailwindcss(),
       react({
       jsxRuntime: "automatic",
     }),
   ],
-  base: process.env.VITE_BASE_URL || "/"
 })
+
